986a45ec610cd23bf44f477cd2843f972804704e
awatersny <awatersny@gmail.com>
2025-03-28 17:23:30 -0400

    Add event listeners to navbar links

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 import * as Data from "./my_modules/data.js"
+import * as Potter from "./my_modules/potter.js"
 
 const navbar = document.getElementById("navbar")
 
-{/* <li class="nav-element">
-  <a href="#"><h2>Nav1</h2></a>
-</li> */}
 async function buildNavBar() {
   const categories = await Data.getCategories()
   categories.forEach(category => {
     const navEl = document.createElement("li")
     navEl.innerHTML = `<a href="#"><h2>${category}</h2></a>`
+    const link = navEl.firstChild.firstChild
     navEl.firstChild.style.textDecoration = "none"
     navEl.firstChild.style.color = "brown"
+    link.setAttribute("id", category.toLowerCase())
+    link.addEventListener("click", renderCategory)
     navbar.appendChild(navEl)
   });
+}
+
+async function renderCategory(evt) {
+  evt.preventDefault()
+  const topic = await Potter.getAll(evt.target.id)
+  console.log(topic)
 }
 
 buildNavBar()
