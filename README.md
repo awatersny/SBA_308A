cd20e5643a366ed28f9458395f19621a4f0e5413
awatersny <awatersny@gmail.com>
2025-03-28 15:09:37 -0400

    Retreive all data from api and update README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Title goes here
+# Diet Harry Potter Database
 Attributions
 --
 - Photo by RDNE Stock project: https://www.pexels.com/photo/close-up-shot-of-a-spell-book-beside-potions-7979069/
--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 import * as Data from "./data.js"
 
-console.log( await Data.getAll() )
+const books = await Data.getAll("books")
+const characters = await Data.getAll("characters")
+const houses = await Data.getAll("houses")
+const spells = await Data.getAll("spells")
+console.log(books)
+console.log(characters)
+console.log(houses)
+console.log(spells)
