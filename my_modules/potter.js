9a046ca99fa4a00544e32aa014b44170c6b94c7c
awatersny <awatersny@gmail.com>
2025-03-28 17:34:44 -0400

    Modify getAll function to accept languages

--- my_modules/potter.js
+++ my_modules/potter.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://potterapi-fedeperin.vercel.app"
 
-export async function getAll(category = '') {
-  const res = await fetch(`${BASE_URL}/en/${category}`)
+export async function getAll(category = '', lang = "en") {
+  const res = await fetch(`${BASE_URL}/${lang}/${category}`)
   const data = await res.json()
   return data
 }
