58657fc30ec45861ccae757c1417994da5166ca2
awatersny <awatersny@gmail.com>
2025-03-28 16:30:18 -0400

    Build a navbar

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import * as Data from "./my_modules/data.js"
 
-console.log(Data.books)
-console.log(Data.characters)
-console.log(Data.houses)
-console.log(Data.spells)
+const navbar = document.getElementById("navbar")
+
+{/* <li class="nav-element">
+  <a href="#"><h2>Nav1</h2></a>
+</li> */}
+async function buildNavBar() {
+  const categories = await Data.getCategories()
+  categories.forEach(category => {
+    const navEl = document.createElement("li")
+    navEl.innerHTML = `<a href="#"><h2>${category}</h2></a>`
+    navEl.firstChild.style.textDecoration = "none"
+    navEl.firstChild.style.color = "brown"
+    navbar.appendChild(navEl)
+  });
+}
+
+buildNavBar()
--- my_modules/data.js
+++ my_modules/data.js
@@ -1,6 +1,12 @@
 import * as Potter from "./potter.js"
 
+export const info = await Potter.getAll()
 export const books = await Potter.getAll("books")
 export const characters = await Potter.getAll("characters")
 export const houses = await Potter.getAll("houses")
 export const spells = await Potter.getAll("spells")
+
+export async function getCategories() {
+  const info = await Potter.getAll()
+  return info.routes.map(route => route.slice(4)[0].toUpperCase()+route.slice(5))
+}
